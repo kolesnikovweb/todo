7115a901a44e7234ea47657833c1907db1a00900
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2023-05-31 21:57:10 +0300

    Нашел элементы на странице

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// const newTask = document.getElementById('new');
+// const addTask = document.getElementById('add');
+// const tasks = document.getElementById('tasks');
+
+// console.log(newTask);
+// console.log(addTask);
+// console.log(tasks);
+
+const dom = {
+    new:  document.getElementById('new'),
+    add: document.getElementById('add'),
+    tasks:  document.getElementById('tasks'),
+}
+
+console.log(dom);
+console.log(1+1);
