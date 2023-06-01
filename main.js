55bb2bfa88eb3ba434745c9cf4a8cf16b6c67f48
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2023-06-01 11:26:28 +0300

    Написал код добавления задачи

--- main.js
+++ main.js
@@ -13,5 +13,45 @@ const dom = {
     tasks:  document.getElementById('tasks'),
 }
 
-console.log(dom);
-console.log(1+1);
+
+const tasks = [];
+
+// Отследить клик по кнопке добавить
+
+dom.add.onclick = () => {
+    const newTaskText = dom.new.value;
+    if(newTaskText && isNotHaveTask(newTaskText, tasks)) {
+        addTask(newTaskText);
+        dom.new.value = ''
+    }
+}
+
+// Функция добавить задачу 
+
+function addTask(text) {
+    let timestamp = Date.now();
+    const task = {
+        id: timestamp,
+        text, 
+        isComplate: false,
+    }
+
+    tasks.push(task)
+    console.log(tasks);
+
+}
+
+// Проверка на повтарение задачи в массиве задач 
+
+
+function isNotHaveTask(text, list) {
+    let isNotHave = true;
+
+    list.forEach((task) => {
+        if(task.text == text) {
+            alert('Задача уже существует!')
+            isNotHave = false    
+        }
+    })
+    return isNotHave
+}
